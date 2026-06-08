7b52e332665b805f36a61b458e9e1f1587d06161
Jawad <jawmat90@kingshigh.school.nz>
2026-06-08 12:23:18 +1200

    killzone and part of titlescreen

---
+++ scenes/killzone.gd
@@ -0,0 +1,10 @@
+extends CollisionShape2D
+
+@onready var timer = $Timer
+
+func _on_body_entered(body):
+	print("you died HAHAHAHA")
+	timer.start()
+	
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
